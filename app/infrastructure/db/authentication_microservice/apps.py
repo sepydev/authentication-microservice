aa58256bb2092f1ea7d9e3a7260dca23d52442fc
sepydev <mrprocs@gmail.com>
2023-03-03 20:33:16 +0330

    Create database infrastructure by using the Django ORM

---
+++ app/infrastructure/db/authentication_microservice/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthenticationMicroserviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authentication_microservice'
