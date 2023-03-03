aa9f02d31ead08a10a2084a01d01b212c9f54ef9
sepydev <mrprocs@gmail.com>
2023-03-03 13:12:18 +0330

    Create User repository by using django orm

---
+++ app/infrastructure/db/repositories/user_repository.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from django.contrib.auth import get_user_model
+from app.domain.entities.user import User
+from app.domain.interface_adapters.repositories.user_repository import UserRepository
+
+UserModel = get_user_model()
+
+
+class UserRepository(UserRepository):
+    def get_user_by_email(self, email: str) -> Optional[User]:
+        try:
+            django_user = UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            return None
+        user = User(
+            id=django_user.id,
+            email=django_user.email,
+            first_name=django_user.first_name,
+            last_name=django_user.last_name,
+            password_hash=django_user.password,
+        )
+        return user
+
+    def create_user(self, user: User) -> User:
+        django_user = UserModel.objects.create_user(
+            username=user.email,
+            email=user.email,
+            password=user.password_hash,
+            first_name=user.first_name,
+            last_name=user.last_name,
+        )
+        user.id = django_user.id
+        return user
