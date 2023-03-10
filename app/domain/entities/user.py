65158f2fee01388f58b4245e3e730647917e620d
sepydev <mrprocs@gmail.com>
2023-03-10 16:48:57 +0330

    Validate data in User class

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -1,13 +1,21 @@
-from dataclasses import dataclass
 from typing import Optional
+
+from pydantic import validator
+from pydantic.dataclasses import dataclass
+
+from ...services.validator import email_validator
 
 
 @dataclass
 class User:
-    id: int
     email: str
     password_hash: str
+    id: Optional[int] = None
     is_active: bool = True
     is_superuser: bool = False
-    first_name: Optional[str] = None,
+    first_name: Optional[str] = None
     last_name: Optional[str] = None
+
+    @validator('email')
+    def email_validator(cls, value):
+        return email_validator.validate(value)
