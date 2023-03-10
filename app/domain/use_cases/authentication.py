b87a7dc2e51bc4b174468e4ddb981a8148a9b36c
sepydev <mrprocs@gmail.com>
2023-03-10 16:49:59 +0330

    Fix issue: Change the logic of authenticate use case

--- app/domain/use_cases/authentication.py
+++ app/domain/use_cases/authentication.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from app.domain.entities.exception import UserOrPasswordIsWrong
+from app.domain.entities.user import User
 from app.domain.interfaces.repositories.user_repository import IUserRepository
 from app.domain.interfaces.services.password_service import IPasswordService
 from app.domain.interfaces.services.token_service import ITokenService
@@ -14,14 +18,20 @@ class AuthenticationUseCase:
         self.token_service = token_service
         self.password_service = password_service
 
-    def authenticate_user(self, email: str, password: str) -> str:
+    def _authenticate_user(self, email: str, password: str) -> Optional[User]:
         user = self.user_repository.get_user_by_email(email)
         if not user or not self.password_service.verify_password(password, user.password_hash):
-            return None
+            raise UserOrPasswordIsWrong()
+        return user
 
-        access_token_expiration = 30  # in minutes
+    def get_token(self, email: str, password: str) -> str:
+        user = self._authenticate_user(email, password)
         access_token = self.token_service.generate_access_token(
-            data={"email": user.email, "user_id": user.id}
-            , expires_delta=access_token_expiration
+            data={"email": user.email, "user_id": user.id},
         )
-        return access_token
+        return {"access_token": access_token, "token_type": "bearer"}
+
+    def get_user_by_token(self, token: str) -> Optional[User]:
+        token_data = self.token_service.verify_access_token(token)
+        user = self.user_repository.get_user_by_id(token_data['user_id'])
+        return user
